d712456a2976150682bc186ad5afd94c5596e24f
huda fahad <hh252ud@gmail.com>
2026-07-27 12:11:57 +0300

    Create Project Structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import BookCard from "./components/BookCard";
+import BookDetails from "./pages/BookDetails";
+import Books from "./pages/Books";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
-function App() {
+ function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Footer/>
+      <BookCard/>
+      <BookDetails/>
+      <Books/>
+      <Home/>
+      <About/>
+      
     </div>
-  );
+  )
 }
-
 export default App;
